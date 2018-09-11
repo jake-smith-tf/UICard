91bdb21c444561b28f0207df2fbec2e096d67579
jake-smith-tf <jake.smith.tf@gmail.com>
2018-09-11 03:28:45 +0100

    Bugfix - return to origin with cancelled touch

--- UICard.swift
+++ UICard.swift
@@ -35,22 +35,15 @@ class UICard: UIView,UIDynamicAnimatorDelegate {
     override init(frame: CGRect) {
         super.init(frame: frame)
         layer.cornerRadius = cornerRadius
-        
     }
     
-    func dynamicAnimatorDidPause(_ animator: UIDynamicAnimator) {
-
-    }
-    
-    func dynamicAnimatorWillResume(_ animator: UIDynamicAnimator) {
-        
-    }
     
     override func awakeFromNib() {
         animator = UIDynamicAnimator(referenceView: self.superview!)
         animator.delegate = self
         initialCenter = self.center
         pan = UIPanGestureRecognizer(target: self, action: #selector(self.panning))
+        pan.maximumNumberOfTouches = 1
         self.addGestureRecognizer(pan)
     }
     
@@ -59,25 +52,25 @@ class UICard: UIView,UIDynamicAnimatorDelegate {
     {
         let panLocationInView = panGesture.location(in: self.superview!)
         let panLocationInCard = panGesture.location(in: self)
-        
-        if panGesture.state == UIGestureRecognizerState.began {
-            animator.removeAllBehaviors()
-            
-            let offset = UIOffsetMake(panLocationInCard.x - self.bounds.midX, panLocationInCard.y - self.bounds.midY);
-            attachment = UIAttachmentBehavior(item: self, offsetFromCenter: offset, attachedToAnchor: panLocationInView)
-            animator.addBehavior(attachment)
-        }
-        
-        if panGesture.state == UIGestureRecognizerState.ended {
-            UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [], animations: {
-                self.animator.removeAllBehaviors()
-                self.transform = CGAffineTransform.init(rotationAngle: 0)
-                self.layoutIfNeeded()
-            }, completion: nil)
-        }
-        
-        if panGesture.state == UIGestureRecognizerState.changed {
-            attachment.anchorPoint = panLocationInView
+        switch (panGesture.state)
+        {
+            case UIGestureRecognizerState.began:
+                animator.removeAllBehaviors()
+                let offset = UIOffsetMake(panLocationInCard.x - self.bounds.midX, panLocationInCard.y - self.bounds.midY);
+                attachment = UIAttachmentBehavior(item: self, offsetFromCenter: offset, attachedToAnchor: panLocationInView)
+                animator.addBehavior(attachment)
+            case UIGestureRecognizerState.changed:
+                attachment.anchorPoint = panLocationInView
+            default:
+                UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [], animations: {
+                    self.animator.removeAllBehaviors()
+                    self.transform = CGAffineTransform.init(rotationAngle: 0)
+                    self.layoutIfNeeded()
+                    if self.translatesAutoresizingMaskIntoConstraints == true
+                    {
+                        self.center = self.initialCenter
+                    }
+                }, completion: nil)
         }
     }
 
