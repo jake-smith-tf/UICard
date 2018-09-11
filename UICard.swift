708670047fb27bbeccc9d43e738c46b488000b9c
jake-smith-tf <jake.smith.tf@gmail.com>
2018-09-11 09:49:43 +0100

    Added delegate methods
    Delegate call whenever card is swiped in a specific direction.

--- UICard.swift
+++ UICard.swift
@@ -16,6 +16,7 @@ class UICard: UIView,UIDynamicAnimatorDelegate {
     var pan:UIPanGestureRecognizer!
     var attachment:UIAttachmentBehavior!
     var initialCenter: CGPoint!
+    var delegate: UICardDelegate?
     
     @IBInspectable var cornerRadius: CGFloat = 10 {
         didSet {
@@ -39,6 +40,14 @@ class UICard: UIView,UIDynamicAnimatorDelegate {
     
     
     override func awakeFromNib() {
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.75
+        layer.shadowOffset = CGSize(width: -1, height: 1)
+        layer.shadowRadius = 1
+        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius).cgPath
+        layer.shouldRasterize = true
+        layer.rasterizationScale = UIScreen.main.scale
         animator = UIDynamicAnimator(referenceView: self.superview!)
         animator.delegate = self
         initialCenter = self.center
@@ -52,6 +61,8 @@ class UICard: UIView,UIDynamicAnimatorDelegate {
     {
         let panLocationInView = panGesture.location(in: self.superview!)
         let panLocationInCard = panGesture.location(in: self)
+        let superWidth = self.superview!.frame.width
+        let superHeight = self.superview!.frame.height
         switch (panGesture.state)
         {
             case UIGestureRecognizerState.began:
@@ -60,12 +71,26 @@ class UICard: UIView,UIDynamicAnimatorDelegate {
                 attachment = UIAttachmentBehavior(item: self, offsetFromCenter: offset, attachedToAnchor: panLocationInView)
                 animator.addBehavior(attachment)
             case UIGestureRecognizerState.changed:
+                alpha = 1 - (abs(panLocationInView.x-superWidth/2))/(superWidth/2)
                 attachment.anchorPoint = panLocationInView
             default:
+                if panLocationInView.x < superWidth*0.25 {
+                    delegate?.swipedLeft(sender: self)
+                }
+                else if panLocationInView.x > superWidth*0.75 {
+                    delegate?.swipedRight(sender: self)
+                }
+                else if panLocationInView.y < superHeight*0.25 {
+                    delegate?.swipedUp(sender: self)
+                }
+                else if panLocationInView.y > superHeight*0.75 {
+                    delegate?.swipedDown(sender: self)
+                }
                 UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [], animations: {
                     self.animator.removeAllBehaviors()
                     self.transform = CGAffineTransform.init(rotationAngle: 0)
                     self.layoutIfNeeded()
+                    self.alpha = 1.0
                     if self.translatesAutoresizingMaskIntoConstraints == true
                     {
                         self.center = self.initialCenter
@@ -96,6 +121,13 @@ class UICard: UIView,UIDynamicAnimatorDelegate {
     }
     
     
+}
+
+protocol UICardDelegate {
+    func swipedRight(sender:UICard)
+    func swipedLeft(sender:UICard)
+    func swipedUp(sender:UICard)
+    func swipedDown(sender:UICard)
 }
 
 
